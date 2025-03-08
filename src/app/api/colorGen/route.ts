2894f159d73107341d2bf184b87529bc97312c0b
unknown <dreicleal@hotmail.com>
2025-03-08 21:06:24 +0000

    Color generation request
    Color generation request with open ai structured respone

--- src/app/api/colorGen/route.ts
+++ src/app/api/colorGen/route.ts
@@ -1,12 +1,14 @@
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
-import { zodResponseFormat } from "openai/src/helpers/zod.js";
+import { zodResponseFormat } from "openai/helpers/zod";
 import { z } from "zod";
 
 const paletteSchema = z.object({
-  reasoning: z.string().max(300),
-  hexes: z.array(z.string()).min(1).max(5),
+  reasoning: z.string().describe("A short portuguese reasoning for the color palette, max 300 characters"),
+  hexes: z.array(z.string()).describe("A list of HEX color codes"),
 });
+
+export type IPaletteSchema = z.infer<typeof paletteSchema>;
 
 const API_KEY = process.env.OPENAI_API_KEY;
 const openai = new OpenAI({ apiKey: API_KEY });
