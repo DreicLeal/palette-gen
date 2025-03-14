74c573573db736106fa12a2d62e20ff04bf82d09
unknown <dreicleal@hotmail.com>
2025-03-14 21:27:09 +0000

    Prompt engineering improvement
    Prompt and structured response schema improved

--- src/app/api/colorGen/route.ts
+++ src/app/api/colorGen/route.ts
@@ -3,11 +3,11 @@ import OpenAI from "openai";
 import { zodResponseFormat } from "openai/helpers/zod";
 import { z } from "zod";
 
-const paletteSchema = z.object({
-  reasoning: z.string().describe("A short portuguese reasoning for the color palette, max 300 characters"),
-  hexes: z.array(z.string()).describe("A list of HEX color codes"),
-});
 
+const paletteSchema = z.object({
+  reasoning: z.string().describe("A very detailed portuguese reasoning for the color palette splitted in bullet points and paragraphs, max. 800 words markdown formatted"),
+  hexes: z.array(z.string()).describe("A list of 5 HEX color codes"),
+});
 export type IPaletteSchema = z.infer<typeof paletteSchema>;
 
 const API_KEY = process.env.OPENAI_API_KEY;
@@ -23,7 +23,7 @@ export async function POST(req: Request) {
         {
           role: "system",
           content:
-            "You are an AI that generates palettes of colors based on the user prompt, always taking in consideration design and colors concepts to match the colors",
+            "You are an AI that generates palettes of colors based on the user prompt, always taking in consideration design and colors concepts to match the colors, explain your colors choices detailed using lists and paragraphs, colors theory and format it in markdown",
         },
         {
           role: "user",
