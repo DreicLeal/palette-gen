da8e1d1d7659f3d9b68878e74b70a2bae7d58484
unknown <dreicleal@hotmail.com>
2025-03-15 18:16:17 +0000

    Fixing ads
    Next metatag for adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ColorsProvider from "@/context/colorsContext";
-import Head from "next/head";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,9 +16,12 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "AI Palette Generation",
-  description: "Create a palette color for your business wuth AI",
+  description: "Create a palette color for your business with AI",
   icons: {
     icon: "/favicon.png",
+  },
+  other: {
+    "google-adsense-account": "ca-pub-2263727171319959"
   },
 };
 
@@ -29,17 +32,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2263727171319959"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
       <ColorsProvider>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <Script
+            async
+            strategy="afterInteractive"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2263727171319959"
+            crossOrigin="anonymous"
+          />
           {children}
         </body>
       </ColorsProvider>
