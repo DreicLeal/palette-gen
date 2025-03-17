70c794d9383392d36456570f64a8f543d4d7daf8
unknown <dreicleal@hotmail.com>
2025-03-17 20:19:34 +0000

    Added React bits components
    Added React bits animated components, and totally reshape the web app

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,39 +1,56 @@
 "use client";
 import { useColors } from "@/context/colorsContext";
+import GradientText from "@/TextAnimations/GradientText/GradientText";
+import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
 
 export default function Form() {
-  const { request} = useColors();
+  const { request } = useColors();
   const [prompt, setPrompt] = useState("");
+  const router = useRouter();
 
   const sendRequest = async (e: FormEvent) => {
-    if (prompt.trim() === "") throw new Error("Need to have something written on the prompt")
+    if (prompt.trim() === "")
+      throw new Error("Need to have something written on the prompt");
+    router.push("/palette");
     e.preventDefault();
     await request(prompt);
-
     setPrompt("");
   };
-  
+
   return (
     <form
       onSubmit={sendRequest}
-      className="bg-white p-2 w-full mx-auto rounded-md flex items-center justify-between gap-2"
+      className="absolute inset-0 flex justify-center items-center"
     >
-      <label htmlFor="prompt" className="text-slate-600 font-bold w-full">
-        Descreva o seu negócio ou a sua ideia:
+      <div
+        className="w-[90%] max-w-lg p-6 rounded-lg shadow-lg flex flex-col gap-4 
+    bg-white/10 backdrop-blur-xs border border-white/40"
+      >
         <textarea
-          className="outline-0 min-w-[100%] h-[200px] bg-slate-200 font-medium p-2 rounded-md"
           id="prompt"
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
+          className="w-full h-[180px] bg-white/30 p-3 rounded-md border text-slate-500 border-slate-300 
+        focus:outline-none focus:ring-2 focus:ring-slate-500 resize-none 
+        placeholder:text-slate-500"
+          placeholder="Exemplo: Tenho uma empresa de marketing digital focada em crescimento orgânico para pequenos negócios..."
         />
-      </label>
-      <button
-        type="submit"
-        className="text-slate-600 bg-slate-200 p-2 rounded-md font-bold"
-      >
-        Enviar
-      </button>
+
+        <button
+          type="submit"
+          className="relative w-full flex justify-center items-center bg-blue rounded-md"
+        >
+          <GradientText
+            colors={["#FF6EC7", "#A26BFA", "#58E6FF", "#32FFD2", "#FFD93D"]}
+            animationSpeed={3}
+            showBorder={true}
+            className="px-20 py-3 text-lg font-semibold"
+          >
+            Enviar
+          </GradientText>
+        </button>
+      </div>
     </form>
   );
 }
